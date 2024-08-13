ca057729cbc127b12d93ac5e1b47f535411b77bb
avmaurya07 <avmaurya07@gmail.com>
2024-08-13 21:01:39 +0530

    initial setup

---
+++ db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const mongoURI = process.env.mongoURI;
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI,()=>{});
+};
+
+module.exports = connectToMongo;
