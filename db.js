fc75fbf24abc369dabf584253d397528a10bf8a8
avmaurya07 <avmaurya07@gmail.com>
2024-08-31 01:03:24 +0530

    minar changes

--- db.js
+++ db.js
@@ -1,16 +1,7 @@
 require('dotenv').config();
-const { MongoClient } = require('mongodb');
-
-const mongoURI = process.env.mongoURI;
-const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const connectToMongo = async () => {
-    try {
-        await client.connect();
-        console.log("Connected to MongoDB");
-    } catch (err) {
-        console.error("Failed to connect to MongoDB", err);
-    }
-};
-
+const mongoose = require('mongoose');
+const mongoURI = process.env.mongoURI 
+const connectToMongo = () => {
+    mongoose.connect(mongoURI)
+}
 module.exports = connectToMongo;
